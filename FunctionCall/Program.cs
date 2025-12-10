6734e3a5d5f980e62bdb819984d979c6cbcf5fdc
The_F <The_F@FIRSTFIDDLER>
2025-12-10 17:25:08 +0300

    Add project files.

---
+++ FunctionCall/Program.cs
@@ -0,0 +1,78 @@
+﻿// get credentials from user secrets
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OllamaSharp;
+using OpenAI;
+using System.ClientModel;
+
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential( config [ "GitHubModels:Token" ] ?? throw new InvalidOperationException( "Missing configuration: GitHubModels:Token." ) );
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri( "https://models.github.ai/inference" )
+};
+
+// create a chat client openai/gpt-5-mini
+IChatClient client =
+    new ChatClientBuilder( new OpenAIClient( credential , options ).GetChatClient( "openai/gpt-4.1-mini" ).AsIChatClient() )
+    .UseFunctionInvocation()
+    .Build();
+
+
+//IChatClient client = 
+//    new ChatClientBuilder( new OllamaApiClient( new Uri( "http://localhost:11434" ) , "llama3.2" ) )
+//    .UseFunctionInvocation()
+//    .Build();
+
+var chatOptions = new ChatOptions
+{
+    Tools = [AIFunctionFactory.Create((string location, string unit) =>
+    {
+        // Here you would call a weather API to get the weather for the location
+        var temperature = Random.Shared.Next(5, 20);
+        var conditions = Random.Shared.Next(0, 1) == 0 ? "sunny" : "rainy";
+
+        return $"The weather is {temperature} degrees C and {conditions}.";
+    },
+    "get_current_weather",
+    "Retrieve the current weather conditions for a specific location. " +
+    "Use this function whenever the user asks about weather, temperature, " +
+    "forecast, climate, or any weather-related information. " +
+    "The function returns the temperature and conditions based on the selected unit.")]
+};
+
+List<ChatMessage> chatHistory = [
+    //new( ChatRole.System , """
+    //You are a hiking enthusiast who helps people discover fun hikes in their area. 
+    //You are upbeat and friendly.
+    //the answer should be shorted in one line and the line maxmuim 30 words and the answer must contain the degrees that return from my system and condition.
+    //""" ) 
+     new(ChatRole.System, """
+        You are a hiking enthusiast who helps people discover fun hikes in their area.
+        You are upbeat and friendly.
+
+        ALWAYS call the tool `get_current_weather` when the user asks about weather.
+        Your answer must:
+        - Be ONE line only, maximum 30 words.
+        - INCLUDE the exact temperature with F dagree and condition text returned by the tool.
+        Do NOT invent your own temperature.
+        """)
+
+    ];
+
+// Weather conversation relevant to the registered function.
+chatHistory.Add( new( ChatRole.User , """
+    I live in Istanbul and I'm looking for a moderate intensity hike. 
+    What's the current weather like? 
+    """ ) );
+
+Console.WriteLine( $"{chatHistory.Last().Role} >>> {chatHistory.Last()}" );
+
+ChatResponse response = await client.GetResponseAsync( chatHistory , chatOptions );
+
+chatHistory.Add( new( ChatRole.Assistant , response.Text ) );
+
+Console.WriteLine( $"{chatHistory.Last().Role} >>> {chatHistory.Last()}" );
+
+Console.ReadKey();
---
+++ VectorSearchOllama/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using OllamaSharp;
+using VectorSearchOllama;
+
+// Create the embedding generator
+IEmbeddingGenerator<string , Embedding<float>> generator =
+    new OllamaApiClient( new Uri( "http://localhost:11434/" ) , "all-minilm" );
+
+
+// Create and populate the vector store
+var vectorStore = new InMemoryVectorStore();
+
+var moviesStore = vectorStore.GetCollection<int , Movie>( "movies" );
+
+await moviesStore.EnsureCollectionExistsAsync();
+
+foreach ( var movie in MovieData.Movies )
+{
+    // generate the embedding vector for the movie description
+    var descEmbedding = await generator.GenerateVectorAsync( movie.Description );
+    movie.DescriptionVector = descEmbedding;
+    // generate the embedding vector for the movie title
+    var titleEmbedding = await generator.GenerateVectorAsync( movie.Title );
+    movie.TitleVector = titleEmbedding;
+
+
+    movie.SearchVector = CombineEmbeddings( titleEmbedding , descEmbedding );
+
+    // add the overall movie to the in-memory vector store's movie collection
+    await moviesStore.UpsertAsync( movie );
+}
+//1-Embed the user’s query
+//2-Vectorized search
+//3-Returns the records
+
+// generate the embedding vector for the user's prompt
+var query = "Lion King is a classic Disney animated film that tells the story of a young lion named Simba";
+//var query = "I want to see family friendly movie";
+//var query = "A science fiction movie about space travel";
+//var query = "Interstellar movie is a science fiction movie about space travel";
+var queryEmbedding = await generator.GenerateVectorAsync( query );
+
+// Choose which vector property to search on:
+var optionSearch = new VectorSearchOptions<Movie>
+{
+    // pick ONE of your vector properties:
+    VectorProperty = m => m.SearchVector ,          // or m => m.ReviewEmbedding
+     Skip = 2,
+    IncludeVectors = true                          // optional: don’t return vectors
+};
+
+
+// search the knowledge store based on the user's prompt
+var searchResults = moviesStore.SearchAsync( queryEmbedding , top: 2 , optionSearch );
+
+
+// see the results just so we know what they look like
+await foreach ( var result in searchResults )
+{
+    Console.WriteLine( $"Title: {result.Record.Title}" );
+    Console.WriteLine( $"Description: {result.Record.Description}" );
+    Console.WriteLine( $"Score: {result.Score}" );
+    Console.WriteLine();
+}
+Console.ReadKey();
+
+ReadOnlyMemory<float> CombineEmbeddings ( ReadOnlyMemory<float> a , ReadOnlyMemory<float> b )
+{
+    var aSpan = a.Span;
+    var bSpan = b.Span;
+
+    if ( aSpan.Length != bSpan.Length )
+        throw new InvalidOperationException( "Embeddings must have same dimension." );
+
+    var combined = new float [ aSpan.Length ];
+    for ( int i = 0 ; i < combined.Length ; i++ )
+    {
+        combined [ i ] = ( aSpan [ i ] + bSpan [ i ] ) / 2f; // average
+    }
+
+    return combined;
+}
